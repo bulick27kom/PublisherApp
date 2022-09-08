d3957e86b28f80dc8f81efd226306fa01099a432
Roman Brauer <bulick27kom@gmail.com>
2022-09-08 09:12:54 +0300

    Add new project

---
+++ PublisherApp/PublisherDomain/Author.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublisherDomain
+{
+    public class Author
+    {
+        public Author()
+        {
+            Books = new Book();
+        }
+
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public List<Book> Books { get; set; }
+
+    }
+}
